47d3e2ead6a53890dff8c4673e8338fe9dd63945
cody <codyjay007@gmail.com>
2026-05-14 00:11:51 -0700

    Configure turtle soup Supabase endpoint

---
+++ turtle-soup/config.js
@@ -0,0 +1,5 @@
+window.TURTLE_CONFIG = {
+  supabaseUrl: "https://rnmqdamcsdhumsmrajoc.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJubXFkYW1jc2RodW1zbXJham9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNjg0OTQsImV4cCI6MjA5Mjc0NDQ5NH0.exgSyjOhgVqynRpnL5dcrY-iQavgI5_-t-L2N8fWUaY",
+  askFunction: "ask-turtle",
+};
